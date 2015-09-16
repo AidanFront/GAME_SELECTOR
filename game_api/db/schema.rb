2151bed84aece137426864050a8bece56b47f275
AIDAN PARTINGTON <AidanPartington@gmail.com>
2015-09-16 13:50:09 +1000

    setup up relationship between game and review tables

--- game_api/db/schema.rb
+++ game_api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916033619) do
+ActiveRecord::Schema.define(version: 20150916034447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150916033619) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "game_id"
   end
 
 end
